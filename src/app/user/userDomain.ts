b6fe25a5d45c5279439dee151bf4fff60b73434a
yogesh147 <yogeshch147@gmail.com>
2021-07-31 17:20:38 +0530

    add app icon

--- src/app/user/userDomain.ts
+++ src/app/user/userDomain.ts
@@ -1,8 +1,22 @@
+
 export class UserDomain {
-  id?: any;
-  firstName?: any;
-  lastName?: any;
-  class?: any;
-  email?: any;
-  userId?: any;
+  id?: string;
+  userId?: string;
+  name?: string;
+  rollNo?: string;
+  grade?: string;
+  salary?: number;
+  age?: number;
+  address?: Address;
+  phoneNos?: string[];
+}
+
+class Address {
+  id?: string;
+  street?: string;
+  houseNo?: string;
+  city?: string;
+  state?: string;
+  country?: string;
+  latLong?: string;
 }
